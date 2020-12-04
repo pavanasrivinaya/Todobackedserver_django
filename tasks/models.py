1defa85eb5e72343b31f66b8948d2951ae397d49
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-12-04 20:47:17 +0530

    first commit

---
+++ tasks/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from utils.models import Timestamps
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+class Task(Timestamps, models.Model):
+    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000, blank=True)
+
+    def __str__(self):
+        return self.title
---
+++ tasks/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from rest_framework import serializers, viewsets
+from .models import Task
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    
+    class Meta:
+        model = Task
+        fields = ['title', 'description', 'user_id', 'created_at', 'updated_at']
+
+# ViewSets define the view behavior.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
