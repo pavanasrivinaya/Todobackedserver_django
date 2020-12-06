027c8f22128a7392207b29aac3b0c0a5f3451674
Pavana sri vinaya <pavanasri.nyros@gmail.com>
2020-12-06 22:32:15 +0530

    model

--- tasks/models.py
+++ tasks/models.py
@@ -4,9 +4,10 @@ from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Task(Timestamps, models.Model):
-    user_id = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
-    description = models.CharField(max_length=1000, blank=True)
+    description = models.CharField(max_length=1000,default='', blank=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
